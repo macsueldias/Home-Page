7bc05a3de27630a713f9be770a8f8c65d393403b
Macsuel Dias <66310986+macsueldias@users.noreply.github.com>
2020-12-14 22:09:34 -0300

    Menu

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".menu-toggle").addEventListener("click", function (e) {
+        e.preventDefault();
+        var navi = document.querySelector(".navigation");
+        var navi_height = navi.clientHeight || navi.offsetHeight;
+        navi.style.display = navi_height ? "none" : "block";
+
+        
+    });
+});
