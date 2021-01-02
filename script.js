83d611fd6d956475853046231ca8bb327fa60980
Macsuel Dias <66310986+macsueldias@users.noreply.github.com>
2021-01-02 16:16:54 -0300

    homepage

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".menu-toggle").addEventListener("click", function (e) {
         e.preventDefault();
         var navi = document.querySelector(".navigation");
         var navi_height = navi.clientHeight || navi.offsetHeight;
-        navi.style.display = navi_height ? "none" : "block";
-
-        
+        navi.style.display = navi_height ? "none" : "block";  
     });
 });
